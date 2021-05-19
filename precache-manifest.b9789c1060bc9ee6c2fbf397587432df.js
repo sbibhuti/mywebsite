4d0657decb3129dd8ac02c6948137d26b88b04a6
Bibhuti <bibhutisunari@gmail.com>
2021-05-20 01:59:47 +0530

    Updates

---
+++ precache-manifest.b9789c1060bc9ee6c2fbf397587432df.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39059d1271926f8757e6c0f0229e838a",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "7bdf5bcf7a9fbf199167",
+    "url": "/mywebsite/static/css/2.61f1384f.chunk.css"
+  },
+  {
+    "revision": "a203c46ee15840edebd7",
+    "url": "/mywebsite/static/css/main.c40545cf.chunk.css"
+  },
+  {
+    "revision": "7bdf5bcf7a9fbf199167",
+    "url": "/mywebsite/static/js/2.faac0f9d.chunk.js"
+  },
+  {
+    "revision": "7e631c984d3541483648b9e5749fadc5",
+    "url": "/mywebsite/static/js/2.faac0f9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a203c46ee15840edebd7",
+    "url": "/mywebsite/static/js/main.8379adc0.chunk.js"
+  },
+  {
+    "revision": "8bfdc81bc68db2f39e17",
+    "url": "/mywebsite/static/js/runtime-main.2e72d8b5.js"
+  },
+  {
+    "revision": "cf64439a0d27cb2f2eb3444249b8a2ad",
+    "url": "/mywebsite/static/media/baisakhi.cf64439a.jpg"
+  },
+  {
+    "revision": "432f262328c2b2edde5cf521a0d0e8d1",
+    "url": "/mywebsite/static/media/bg1.432f2623.jpg"
+  },
+  {
+    "revision": "7c8da62fa04bc317d4ae61fd0c2d74d6",
+    "url": "/mywebsite/static/media/dalma.7c8da62f.jpg"
+  },
+  {
+    "revision": "7e9df4ee3adbbbc78b4474453d5d2042",
+    "url": "/mywebsite/static/media/gacr.7e9df4ee.jpg"
+  },
+  {
+    "revision": "f2a37d0ddc6f85c66852aa10ceb9fde6",
+    "url": "/mywebsite/static/media/home.f2a37d0d.svg"
+  },
+  {
+    "revision": "508b6cf2742fe179ad93f7e558db99df",
+    "url": "/mywebsite/static/media/kalinga.508b6cf2.jpg"
+  },
+  {
+    "revision": "beb27bef7955abd8be732df95fc43d89",
+    "url": "/mywebsite/static/media/logo.beb27bef.png"
+  },
+  {
+    "revision": "b10a1bd1b993592f7d9e240650390533",
+    "url": "/mywebsite/static/media/rit.b10a1bd1.jpg"
+  },
+  {
+    "revision": "987136499184946d5ac53fbee8e99af7",
+    "url": "/mywebsite/static/media/subhansi.98713649.jpg"
+  }
+]);
